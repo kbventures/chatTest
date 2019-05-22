c2fa4097e55df20969c122d29ca03da74b7b4ee7
kb <KMB_X@hotmail.com>
2019-05-22 13:55:36 -0400

    meeting with gusman

--- src/sockets/index.js
+++ src/sockets/index.js
@@ -1,32 +1,37 @@
-import *  as types from '../constants/ActionTypes'
-import {addUser, messageReceived, populateUsersList} from '../actions'
+import * as types from '../constants/ActionTypes';
+import { addUser, messageReceived, populateUsersList } from '../actions';
 
 const setupSocket = (dispatch, username) => {
-	const socket = new WebSocket('ws://localhost:8989')
+  const socket = new WebSocket('ws://stark-dawn-81542.herokuapp.com');
 
-	socket.onopen = () => {
-		socket.send(JSON.stringify({
-			type: types.ADD_USER,
-			name: username
-		}))
-	}
-	socket.onmessage = (event) => {
-		const data = JSON.parse(event.data)
-		switch (data.type) {
-			case types.ADD_MESSAGE:
-				dispatch(messageReceived(data.message, data.author))
-				break
-			case types.ADD_USER:
-				dispatch(addUser(data.name))
-				break
-			case types.USERS_LIST:
-				dispatch(populateUsersList(data.users))
-				break
-			default:
-				break
-		}
-	}
-	return socket
-}
+  //const HOST = location.origin.replace(/^http/, 'ws');
+  //const socket = new WebSocket(HOST);
 
-export default setupSocket
+  socket.onopen = () => {
+    socket.send(
+      JSON.stringify({
+        type: types.ADD_USER,
+        name: username
+      })
+    );
+  };
+  socket.onmessage = event => {
+    const data = JSON.parse(event.data);
+    switch (data.type) {
+      case types.ADD_MESSAGE:
+        dispatch(messageReceived(data.message, data.author));
+        break;
+      case types.ADD_USER:
+        dispatch(addUser(data.name));
+        break;
+      case types.USERS_LIST:
+        dispatch(populateUsersList(data.users));
+        break;
+      default:
+        break;
+    }
+  };
+  return socket;
+};
+
+export default setupSocket;
