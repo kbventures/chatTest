fe366f82a2307bdff3f6d25fae45c822b87fdf6e
kmbeaudin <kmbeaudin@prognose.ca>
2019-05-23 09:44:58 -0400

    bla

--- src/sockets/index.js
+++ src/sockets/index.js
@@ -2,7 +2,8 @@ import * as types from '../constants/ActionTypes';
 import { addUser, messageReceived, populateUsersList } from '../actions';
 
 const setupSocket = (dispatch, username) => {
-  const socket = new WebSocket('wss://boiling-brook-76849.herokuapp.com');
+  const socket = new WebSocket('ws://localhost:8989/');
+  //const socket = new WebSocket('wss://boiling-brook-76849.herokuapp.com');
 
   //const HOST = location.origin.replace(/^http/, 'ws');
   //const socket = new WebSocket(HOST);
